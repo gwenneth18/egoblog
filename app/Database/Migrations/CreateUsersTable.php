e993a063c5b5c26acc9ca6046a0de1c734b3fa57
egoblog <e.g.ozakpolor@wlv.ac.uk>
2025-03-15 09:22:10 +0000

    initial commit

---
+++ app/Database/Migrations/CreateUsersTable.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'picture' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+            ],
+            'bio' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'default' => 'CURRENT_TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'default' => 'CURRENT_TIMESTAMP',
+                'null' => true, // Allows CodeIgniter to update it
+            ],
+        ]);
+
+        $this->forge->addKey('id', true); // Set primary key
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = array(
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'username' => 'admin',
+            'password' => password_hash('12345', PASSWORD_BCRYPT), // Fixed comma and correct PASSWORD_BCRYPT usage
+        );
+
+        // Insert the data into the users table
+        $this->db->table('users')->insert($data);
+    }
+}
