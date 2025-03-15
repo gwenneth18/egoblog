57c845f86588bf95c78b83fb5719610015279905
egoblog <e.g.ozakpolor@wlv.ac.uk>
2025-03-15 12:28:48 +0000

    Add Auth and Admin controllers, update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,10 +10,13 @@ $routes->get('/', 'Home::index');
 $routes->group('admin', static function($routes){
 
     $routes->group('',[], static function($routes){
-        $routes->view('example-page','example-page');
+        // $routes->view('example-page','example-page'); 
+        $routes->get('home','AdminController::index', ['as'=> 'admin.home']);
+        
     });
     $routes->group('',[], static function($routes){
-        $routes->view('example-auth','example-auth');
+       // $routes->view('example-auth','example-auth');
+        $routes->get('login','AuthController::loginform', ['as'=> 'admin.login.form']);
     });
 
 
---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuthController extends BaseController
+{
+    protected $helpers = ['url','form'];
+    public function loginform()
+    {
+       $data = [
+        'pageTitle'=> 'Login',
+        'validation'=>null
+       ];
+       return view('backend/pages/auth/login', $data);
+    }
+}
